7593730e2f708b2e5fb8d0830a00466ed1b28bbb
Soresu <g_mester@freemail.hu>
2015-05-04 19:10:08 +0200

    ok

--- UnderratedAIO/UnderratedAIO/Program.cs
+++ UnderratedAIO/UnderratedAIO/Program.cs
@@ -6,16 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
-using UnderratedAIO.Champions;
 
 namespace UnderratedAIO
 {
-    internal class Program
+    class Program
     {
         public static Obj_AI_Hero player = ObjectManager.Player;
         public static string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += OnGameLoad;
         }
@@ -24,51 +22,20 @@ namespace UnderratedAIO
         {
             try
             {
-                switch (player.ChampionName)
+                var type = Type.GetType("UnderratedAIO.Champions." + player.ChampionName);
+                if (type != null)
                 {
-                    case "ChoGath":
-                        Chogath Chogath = new Chogath();
-                        break;
-                    case "Evelynn":
-                        Evelynn Evelynn = new Evelynn();
-                        break;
-                    case "Fiora":
-                        Fiora Fiora = new Fiora();
-                        break;
-                    case "Garen":
-                        Garen Garen = new Garen();
-                        break;
-                    case "Kennen":
-                        Kennen Kennen = new Kennen();
-                        break;
-                    case "Maokai":
-                        Maokai Maokai = new Maokai();
-                        break;
-                    case "Mordekaiser":
-                        Mordekaiser Mordekaiser = new Mordekaiser();
-                        break;
-                    case "Poppy":
-                        Poppy Poppy = new Poppy();
-                        break;
-                    case "Renekton":
-                        Renekton Renekton = new Renekton();
-                        break;
-                    case "Sejuani":
-                        Sejuani Sejuani = new Sejuani();
-                        break;
-                    case "Shen":
-                        Shen Shen = new Shen();
-                        break;
-                    case "Volibear":
-                        Volibear Volibear = new Volibear();
-                        break;
-                    case "Yorick":
-                        Yorick Yorick = new Yorick();
-                        break;
-                    default:
-                        Other Other = new Other();
-                        break;
+                    Helpers.DynamicInitializer.NewInstance(type);
                 }
+                else
+                {
+                    var common = Type.GetType("UnderratedAIO.Champions." + "Other");
+                    if (common != null)
+                    {
+                        Helpers.DynamicInitializer.NewInstance(common);
+                    }
+                }
+
             }
             catch (Exception e)
             {
